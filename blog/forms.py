e26d4ef60f4ab7ecfbee0a5bc1d9cdb006fb0d53
Anna Grigoreva <anstepakova@yandex.ru>
2021-02-06 14:56:45 +0300

    Fix bug with touching database from static context in blog/forms.py which leads to migration failure on empty database

--- blog/forms.py
+++ blog/forms.py
@@ -1,46 +1,48 @@
 from django import forms
 from .models import BlogPost, Category
 
-choices = Category.objects.all().values_list('name', 'name')    # a specific format for choices
-choice_list = []
-for item in choices:
-    choice_list.append(item)
+
+def get_choise_list():
+    return [item for item in Category.objects.all().values_list('name', 'name')]
 
 
 class BlogPostForm(forms.ModelForm):
     class Meta:
-        model = BlogPost
-        fields = ['author', 'category', 'tag', 'title', 'header_img', 'snippet', 'text']
+        def __init__(self):
+            self.model = BlogPost
+            self.fields = ['author', 'category', 'tag', 'title', 'header_img', 'snippet', 'text']
 
-        widgets = {
-            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'member', 'type': 'hidden'}),
-            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
-            'tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'title': forms.TextInput(attrs={'class': 'form-control'}),  # form-control is a css class
-            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
-            'text': forms.Textarea(attrs={'class': 'form-control'}),
-        }
+            self.widgets = {
+                'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'member', 'type': 'hidden'}),
+                'category': forms.Select(choices=get_choise_list(), attrs={'class': 'form-control'}),
+                'tag': forms.TextInput(attrs={'class': 'form-control'}),
+                'title': forms.TextInput(attrs={'class': 'form-control'}),  # form-control is a css class
+                'snippet': forms.Textarea(attrs={'class': 'form-control'}),
+                'text': forms.Textarea(attrs={'class': 'form-control'}),
+            }
 
 
 class UpdateBlogPostForm(forms.ModelForm):
     class Meta:
-        model = BlogPost
-        fields = ['category', 'tag', 'title', 'snippet', 'text']
+        def __init__(self):
+            self.model = BlogPost
+            self.fields = ['category', 'tag', 'title', 'snippet', 'text']
 
-        widgets = {
-            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
-            'tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'title': forms.TextInput(attrs={'class': 'form-control'}),  # form-control is a css class
-            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
-            'text': forms.Textarea(attrs={'class': 'form-control'}),
-        }
+            self.widgets = {
+                'category': forms.Select(choices=get_choise_list(), attrs={'class': 'form-control'}),
+                'tag': forms.TextInput(attrs={'class': 'form-control'}),
+                'title': forms.TextInput(attrs={'class': 'form-control'}),  # form-control is a css class
+                'snippet': forms.Textarea(attrs={'class': 'form-control'}),
+                'text': forms.Textarea(attrs={'class': 'form-control'}),
+            }
 
 
 class AddCategoryForm(forms.ModelForm):
     class Meta:
-        model = Category
-        fields = ['name']
+        def __init__(self):
+            self.model = Category
+            self.fields = ['name']
 
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'})
-        }
+            self.widgets = {
+                'name': forms.TextInput(attrs={'class': 'form-control'})
+            }
