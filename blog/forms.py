73feaaa7838e73035d12529ab287cf4f0ed5ba43
Anna Grigoreva <anstepakova@yandex.ru>
2021-02-08 22:00:55 +0300

    Add ModelChoiceField for category field in forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,63 +1,38 @@
 from django import forms
 from .models import BlogPost, Category, Comment
-# git commit -m "Revert changes in forms.py (otherwise nothing worked)"
-
-def get_choise_list():
-    return [item for item in Category.objects.all().values_list('name', 'name')]
 
 
 class BlogPostForm(forms.ModelForm):
+    category = forms.ModelChoiceField(queryset=Category.objects, to_field_name="name",
+                                      widget=forms.Select(attrs={'class': 'form-control'}))
+
     class Meta:
         model = BlogPost
         fields = ['author', 'category', 'tag', 'title', 'header_img', 'snippet', 'text']
 
         widgets = {
             'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'member', 'type': 'hidden'}),
-            'category': forms.Select(choices=get_choise_list(), attrs={'class': 'form-control'}),
             'tag': forms.TextInput(attrs={'class': 'form-control'}),
             'title': forms.TextInput(attrs={'class': 'form-control'}),  # form-control is a css class
             'snippet': forms.Textarea(attrs={'class': 'form-control'}),
             'text': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
-        # def __init__(self):
-        #     self.model = BlogPost
-        #     self.fields = ['author', 'category', 'tag', 'title', 'header_img', 'snippet', 'text']
-        #
-        #     self.widgets = {
-        #         'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'member', 'type': 'hidden'}),
-        #         'category': forms.Select(choices=get_choise_list(), attrs={'class': 'form-control'}),
-        #         'tag': forms.TextInput(attrs={'class': 'form-control'}),
-        #         'title': forms.TextInput(attrs={'class': 'form-control'}),  # form-control is a css class
-        #         'snippet': forms.Textarea(attrs={'class': 'form-control'}),
-        #         'text': forms.Textarea(attrs={'class': 'form-control'}),
-        #     }
-
 
 class UpdateBlogPostForm(forms.ModelForm):
+    category = forms.ModelChoiceField(queryset=Category.objects, to_field_name="name",
+                                      widget=forms.Select(attrs={'class': 'form-control'}))
+
     class Meta:
         model = BlogPost
         fields = ['category', 'tag', 'title', 'snippet', 'text']
 
         widgets = {
-            'category': forms.Select(choices=get_choise_list(), attrs={'class': 'form-control'}),
             'tag': forms.TextInput(attrs={'class': 'form-control'}),
             'title': forms.TextInput(attrs={'class': 'form-control'}),  # form-control is a css class
             'snippet': forms.Textarea(attrs={'class': 'form-control'}),
             'text': forms.Textarea(attrs={'class': 'form-control'}),
         }
-
-        # def __init__(self):
-        #     self.model = BlogPost
-        #     self.fields = ['category', 'tag', 'title', 'snippet', 'text']
-        #
-        #     self.widgets = {
-        #         'category': forms.Select(choices=get_choise_list(), attrs={'class': 'form-control'}),
-        #         'tag': forms.TextInput(attrs={'class': 'form-control'}),
-        #         'title': forms.TextInput(attrs={'class': 'form-control'}),  # form-control is a css class
-        #         'snippet': forms.Textarea(attrs={'class': 'form-control'}),
-        #         'text': forms.Textarea(attrs={'class': 'form-control'}),
-        #     }
 
 
 class AddCategoryForm(forms.ModelForm):
@@ -68,14 +43,6 @@ class AddCategoryForm(forms.ModelForm):
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'})
         }
-
-        # def __init__(self):
-        #     self.model = Category
-        #     self.fields = ['name']
-        #
-        #     self.widgets = {
-        #         'name': forms.TextInput(attrs={'class': 'form-control'})
-        #     }
 
 
 class CommentForm(forms.ModelForm):
