58a775017e665f4ea6c56d4687f3eb447750e910
Anna Grigoreva <anstepakova@yandex.ru>
2021-02-10 14:08:46 +0300

    Update STATICFILES_STORAGE

--- DPro/settings.py
+++ DPro/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
